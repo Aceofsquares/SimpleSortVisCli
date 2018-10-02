bbaa636496d2c65fb66b4172b014c252a0255c56
Brian Rogers <btcr1990@gmail.com>
2018-10-01 23:11:35 -0500

    Sorts implemented with breakdown

--- clisort.py
+++ clisort.py
@@ -1,11 +1,79 @@
-from random import randint
+from random import randint, choice
 from argparse import ArgumentParser
 from sys import exit
 
+def median(arr):
+    f = arr[0]
+    s = arr[len(arr)//2]
+    t = arr[len(arr)-1]
+    return max(min(f, s), min(max(f, s), t))
+
+def qsort(arr):
+    if len(arr) <= 1:
+        return arr
+    temp = [v for v in arr]
+    pivot = median(temp)
+    temp.remove(pivot)
+
+    less_than = []
+    greater_than = []
+    equal_to = [pivot]
+    for e in temp:
+        if e < pivot:
+            less_than.append(e)
+        elif e > pivot:
+            greater_than.append(e)
+        else:
+            equal_to.append(e)
+    print(f"Pivot: {pivot}")
+    print(f"Less than {pivot}: {less_than}")
+    sorted_less = qsort(less_than)
+    print(f"Sorted Less than: {sorted_less}")
+    print(f"{sorted_less} + {equal_to}: {sorted_less + equal_to}")
+    sorted_less += equal_to
+    print(f"Greater than {pivot}: {greater_than}")
+    sorted_greater = qsort(greater_than)
+    print(f"{sorted_less} + {sorted_greater}: ", end='')
+    print(sorted_less + sorted_greater, end="\n\n")
+    return sorted_less + sorted_greater
+    
+def mergesort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    bot = arr[:len(arr)//2]
+    top = arr[len(arr)//2:]
+
+    print(f"Split {arr} -> {bot} + {top}")
+    bot = mergesort(bot)
+    top = mergesort(top)
+
+    return merge(bot, top)
+
+def merge(arr1, arr2):
+    merged = []
+    idx1 = 0
+    idx2 = 0
+    print(f"{arr1} merge with {arr2}")
+    while idx1 < len(arr1) and idx2 < len(arr2):
+        if idx2 < len(arr2) and arr1[idx1] <= arr2[idx2]:
+            merged.append(arr1[idx1])
+            idx1 += 1
+        if idx1 < len(arr1) and arr2[idx2] < arr1[idx1]:
+            merged.append(arr2[idx2])
+            idx2 += 1
+    while idx1 < len(arr1):
+        merged.append(arr1[idx1])
+        idx1 += 1
+    while idx2 < len(arr2):
+        merged.append(arr2[idx2])
+        idx2 += 1
+    print(f"Merged {merged}")
+    return merged
 
 parser = ArgumentParser()
 
-#parser.add_argument('sortmode', type=str, default="quick", help="Sort mode. Default: quick.  Options: quick, merge")
+parser.add_argument('sortmode', type=str, default="quick", help="Sort mode. Default: quick.  Options: quick, merge")
 parser.add_argument('amount', type=int, help="Amount of integers in list")
 parser.add_argument('-m', '--min', type=int, default=0, help="Minimum value of an integer. Default: 0")
 parser.add_argument('-x', '--max', type=int, default=1000, help="Maximum value of an integer. Default: 1000")
@@ -22,4 +90,14 @@ if args.min > args.max:
 
 values = [randint(args.min, args.max) for _ in range(args.amount)]
 
-print(values)
+print("Unsorted values: " + ', '.join(map(str, values)))
+
+if args.sortmode in ("quick", "q"):
+    sorted_list = qsort(values)
+elif args.sortmode in ("merge", "m"):
+    sorted_list = mergesort(values)
+else:
+    print("Sort modes available: Quick and Merge")
+    exit(3)
+
+print("Sorted values: " + ', '.join(map(str, sorted_list)))
